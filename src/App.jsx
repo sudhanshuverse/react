aaf339035b0d5fa450df408d02a7d084be4ce0f1
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-10-25 14:15:56 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 function App() {
     return (
         <>
-            <h1>Hello </h1>
+            <h1>Hello bro</h1>
+            <h1>How are you</h1>
         </>
     )
 }
