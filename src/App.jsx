e8664e16dc3f16da16d0160d5ea8c22c9a2da988
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-10-25 15:25:01 +0530

    Code Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <h1>Hello bro</h1>
-            <h1>How are you</h1>
+            <h1>Checking the revert previous code</h1>
         </>
     )
 }
