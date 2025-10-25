99f002657aae3a473d304cf1768f44cea0dc2bd3
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-10-25 15:29:50 +0530

    Merged

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from "react"
 function App() {
     return (
         <>
-            <h1>Hello </h1>
+            <h1>Hello bro</h1>
+            <h1>How are you</h1>
+            <h1>This is the testing of revert command</h1>
         </>
     )
 }
